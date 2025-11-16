8a8d9ba73d528ef4df7f12d8736aa30ec4cae907
HumayunAhmed20 <anikhumayun110@gmail.com>
2025-11-16 13:09:26 +0600

    add

--- MID/test1.php
+++ MID/test1.php
@@ -1,108 +1,42 @@
 <!DOCTYPE html>
+ 
 <html>
-  <head>
-    <title>Clinic Patient Registration</title>
-  </head>
-  <body style="font-family: Arial, sans-serif; background-color: white;">
-    <center>
-      <h1 style="margin-top: 30px; color:darkblue;">Clinic Patient Registration</h1>
-      <br>
+<body>
+<h1>This is our JS Code Sample </h1>
+<p> This is the Lab Class</p>
+<p id="demo"></p>
  
-      <table style="background-color: white; padding: 30px; border-radius: 10px; width: 400px;">
-       
-        <tr>
-          <td style="font-weight: bold;">Full Name:</td>
-        </tr>
-        <tr>
-          <td><input type="text" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
+        <script>
+     document.write("Hello World");
+     console.log("Hello World ,I am from Console");
  
-       
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Age:</td>
-        </tr>
-        <tr>
-          <td><input type="text" style="width: 100%; padding: 8px; border: 1px solid ; border-radius: 5px;"></td>
-        </tr>
+     //alert("Welcome to Web Tech Class");
  
-       
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Phone Number:</td>
-        </tr>
-        <tr>
-          <td><input type="tel" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
+var name="Mr. Kaisha";
+var age=60;
+var is_human=true;
+var x;
+var y=null;
  
-       
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Email Address:</td>
-        </tr>
-        <tr>
-          <td><input type="email" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
+console.log("the name is: ",name);
+console.log("the age is: ",age);
+console.log("Human or not : ",is_human);
  
-     
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Insurance Provider:</td>
-        </tr>
-        <tr>
-          <td>
-            <select style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;">
-              <option>Select Provider</option>
-              <option>Provider A</option>
-              <option>Provider B</option>
-              <option>Provider C</option>
-            </select>
-          </td>
-        </tr>
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof is_human);
+console.log(typeof x);
+console.log(typeof y);
  
-       
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Insurance Policy Number:</td>
-        </tr>
-        <tr>
-          <td><input type="text" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
- 
-     
-        <tr>
-          <td><center><h3 style="margin-top: 20px; color:darkblue;">Additional Information</h3></center></td>
-        </tr>
- 
-     
-        <tr>
-          <td style="font-weight: bold;">Username:</td>
-        </tr>
-        <tr>
-          <td><input type="text" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
- 
-       
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Password:</td>
-        </tr>
-        <tr>
-          <td><input type="password" style="width: 100%; padding: 8px; border: 1px solid  black; border-radius: 5px;"></td>
-        </tr>
+var x=10;
+var y=50;
+console.log("The sum is: ",x+y);
+  console.log("The mul is: ",x*y);
+console.log("The div is: ",x/y);
  
  
-        <tr>
-          <td style="font-weight: bold; padding-top: 10px;">Confirm Password:</td>
-        </tr>
-        <tr>
-          <td><input type="password" style="width: 100%; padding: 8px; border: 1px solid black; border-radius: 5px;"></td>
-        </tr>
+        </script>
  
-     
-        <tr>
-          <td style="text-align: full; padding-top: 20px;">
-            <button type="submit" style="width: 380px; background-color: darkblue; color: white; padding: 10px 30px; border: none; border-radius: 5px; cursor: pointer;">
-              Register
-            </button>
-          </td>
-        </tr>
-      </table>
-    </center>
-  </body>
+         
+</body>
 </html>
- 
--- test.php
+++ test.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>JS Lab Class</title>
 </head>
 <body>
 
 <h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<script>
+document.write("Hello Worked");
+console.log("Hello, I am console");
+alert("Hello Class");
+
+var name="Mr. Anik";
+var age=30;
+var is_human=true;
+var x;
+
+console.log("The type of name: ",typeof name);
+console.log("The type of name: ",typeof age);
+console.log("The type of name: ",typeof is_human);
+console.log("The type of x: ",typeof x);
+
+console.log("The name is: :",name);
+var num_1=50;
+var num_2=50;
+var sum= num_1+num_2;
+var div= num_1/num_2;
+console.log("The sum is: ",sum);
+console.log("The div is: ",div);
+console.log("The sub is: ",num_1-num_2);
+
+</script>
+
 
 </body>
 </html>
